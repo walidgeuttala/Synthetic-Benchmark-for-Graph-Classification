bbaef727f6a1a1b887ea76e372a9ab39e6867745
walidguettala <walidguettala@gmail.com>
2023-10-14 11:01:20 +0200

    fix recurresntio problem

--- utils.py
+++ utils.py
@@ -216,9 +216,10 @@ def generate_factors(n, min, max):
     factors = []
     
     # Find factors of n
-    for i in range(n//3+1, n // 2):
-        if n % i == 0:
+    for i in range(4, n // 2):
+        if n % i == 0 and n/i > 3:
             factors.append(i)
+            
     
     # Randomly choose one of the factors as 'a'
     if len(factors) < 2:
